49c2cb3a3075c7d9fb96786f62c0a3516700d72d
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-06-05 18:43:33 +0300

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -6,9 +6,8 @@ Created on Sun May  9 16:26:28 2021
 """
 
 
-import os
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+import os 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -16,48 +15,55 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report,mean_absolute_error,mean_squared_error
+from sklearn.metrics import accuracy_score, classification_report, mean_absolute_error, mean_squared_error
+from sklearn.svm import SVR
+
+#Verisetinin okunması
+
+df = pd.read_csv(r'covid19_by_country.csv')
+
+#Verisetimizde tahminimizi ülkelere göre yapmamız gerekiyor bu yüzden kullanıcıdan
+#Ülke kodu girdisi alıp buna göre verilerimizi tek ülke biçiminde filtreleriz
+
+dateCols = ['CountryAlpha3Code']
+
+ulke = input('Tahmin etmek istediğiniz ülke kodunu giriniz: ')
+
+df = pd.read_csv("covid19_by_country.csv", parse_dates=dateCols)
+
+data=df[(df['CountryAlpha3Code']  ==  ulke)]
+
+# verisetimizdeki null veya değeri verilmemiş değişkenler ihmal edilmelidir.
+
+data = data.dropna() 
+
+# verisetimizi 2 diziye bölme işlemi:
+# eğer type hatası verirse Y ve y'nin sonuna .astype(float) ekleyin 
+
+X = data.drop(['Country','CountryAlpha3Code','deaths','Date','deaths_PopPct',
+               'confirmed_PopPct','DaysSince100Cases','DaysSince1Cases',
+               'GRTStringencyIndex','recoveries_PopPct','recoveries_inc',
+               'deaths_inc','confirmed_inc'], axis=1)
+y = data[['deaths']]
 
 
-os.chdir('G:\Okul\Bitirme2')
-df = pd.read_csv(r'afg.csv')
-""" 
-vaka kurtulan sayısını X verisetimiz için , ölüm sayısını da tahmin
-edeceğimizden dolayı y dizimize ekleyelim
-"""
 
-"""
-grafik
-"""
-corelation = df.corr()
-plt.subplots(figsize=(20,15))
-a = sns.heatmap(corelation,annot=True)
-
-#x= np.array(df.drop(['deaths'],1))
-#y= np.array(df[output])
-
-X = df.iloc[:, [3,5]].values
-y = df.iloc[:, 4].values
-
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(X,y)
 
 
-
-
-#SVC
-svc=SVC(kernel='linear', random_state = 0)
-
+svc = SVC(gamma='auto')
 
 svc.fit(x_train,y_train)
 
-y_pred3=svc.predict(x_test)
 
-accuracy_score(y_test,y_pred3)
+y_pred3 = svc.predict(x_test)
+
+
+accuracy_score(y_test, y_pred3)
 
 
 print(f"Test Set Accuracy : {accuracy_score(y_test,y_pred3) * 100} %\n\n")
-
-print(f"Classification Report : \n\n{classification_report(y_test, y_pred3)}")
+print(f"Classification Report : \n\n {classification_report(y_test, y_pred3)}")
 
 
 
