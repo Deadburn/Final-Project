a15090ba95e80ea4698ea13ef415090e85efbf14
Deadburn <80601286+Deadburn@users.noreply.github.com>
2021-06-09 13:12:14 +0300

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -17,6 +17,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, mean_absolute_error, mean_squared_error
 from sklearn.svm import SVR
+from sklearn import metrics
 
 #Verisetinin okunması
 
@@ -48,24 +49,44 @@ y = data[['deaths']]
 
 
 
-x_train, x_test, y_train, y_test = train_test_split(X,y)
+x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.25)
 
 
-svc = SVC(gamma='auto')
+svc = SVC()
 
 svc.fit(x_train,y_train)
 
 
-y_pred3 = svc.predict(x_test)
+y_pred = svc.predict(x_test)
 
 
-accuracy_score(y_test, y_pred3)
+accuracy_score(y_test, y_pred)
 
 
-print(f"Test Set Accuracy : {accuracy_score(y_test,y_pred3) * 100} %\n\n")
-print(f"Classification Report : \n\n {classification_report(y_test, y_pred3)}")
+print(f"Test Set Accuracy : {accuracy_score(y_test,y_pred) * 100} %\n\n")
+print(f"Classification Report : \n\n {classification_report(y_test, y_pred)}")
+print("R2 score\n")
+print(metrics.r2_score(exp, y_pred))
+print("Mean Squared Log Error\n")
+print(metrics.mean_squared_log_error(exp, y_pred))
+print("Explained Variance Score\n")
+print(metrics.explained_variance_score(y_pred,y_test))
+print("Mean Absolute Error\n")
+print(metrics.mean_absolute_error(exp, y_pred))
 
+## Örnek olarak AUS kodunu girdiğimizde 
+## Test Set Accuracy : 39.83050847457627 %
 
+## R2 score
+## 0.9697156186323612
 
+## Mean Squared Log Error
+## 0.35953020745484654
+
+## Explained Variance Score
+## 0.9720400269443831
+
+## Mean Absolute Error
+## 24.0
 
 
